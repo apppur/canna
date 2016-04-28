2ef03745aad55692df0c4c8d8bb8cf7e3d02b6b5
apppur <applepurplediandian@gmail.com>
2016-04-28 16:30:17 +0800

    add message exchange

--- dbserver/main.cpp
+++ dbserver/main.cpp
@@ -16,6 +16,9 @@
 #include "zmq.hpp"
 
 #define SERVER_INIT_DAEMON 0
+#define COMMAND_LEN 256
+
+char command[COMMAND_LEN];
 
 int initialize(int mode);
 int initdaemon(int mode);
@@ -26,40 +29,52 @@ int main(int argc, char **argv)
 {
     initialize(1);
 
+    int major, minor, patch;
+    zmq::version(&major, &minor, &patch);
+    printf("current zeromq version is %d.%d.%d\n", major, minor, patch);
+
     redisContext *context = redisConnect("127.0.0.1", 6379);
     if (context == nullptr || context->err) {
         if (context) {
             printf("Error: %s, Code: %d\n", context->errstr, context->err);
+            return 1;
         } else {
             printf("Can't allocate redis context\n");
+            return 1;
         }
     } else {
         printf("Success: context init \n");
     }
 
-    redisReply *reply = (redisReply *)redisCommand(context, "SET foo barbarbar");
+    //redisReply *reply = (redisReply *)redisCommand(context, "SET foo barbarbar");
 
 
-        //  Prepare our context and socket
-        zmq::context_t zmq_context (1);
-        zmq::socket_t socket (zmq_context, ZMQ_REP);
-        socket.bind ("tcp://*:5555");
-        
-        while (true) {
-            zmq::message_t request;
+    //  Prepare our context and socket
+    zmq::context_t zmq_context (1);
+    zmq::socket_t socket (zmq_context, ZMQ_REP);
+    socket.bind ("tcp://*:5555");
 
-            //  Wait for next request from client
-            socket.recv (&request);
-            std::cout << "Received Hello" << std::endl;
+    while (true) {
+        zmq::message_t request;
 
-            //  Do some 'work'
-            sleep(1);
-
-            //  Send reply back to client
-            zmq::message_t reply (5);
-            memcpy (reply.data(), "World", 5);
-            socket.send(reply);
+        //  Wait for next request from client
+        socket.recv (&request);
+        if (request.size() > COMMAND_LEN) {
+            continue;
         }
+
+        memset(command, 0, COMMAND_LEN);
+        memcpy(command, static_cast<char*>(request.data()), request.size());
+
+        redisReply *result = (redisReply *)redisCommand(context, command);
+
+        //  Send reply back to client
+        zmq::message_t reply(result->len);
+        memcpy (reply.data(), result->str, result->len);
+        socket.send(reply);
+
+        freeReplyObject(result);
+    }
         return 0;
 }
 
