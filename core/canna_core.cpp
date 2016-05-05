01e530dd6bab4d6f4a32c93d3902d84f694e3898
apppur <applepurplediandian@gmail.com>
2016-05-05 16:02:06 +0800

    zmq recv send wrapper

--- core/canna_core.cpp
+++ core/canna_core.cpp
@@ -1,4 +1,8 @@
 #include <sys/time.h>
+
+#include <iostream>
+#include <sstream>
+#include <iomanip>
 #include "canna_core.h"
 
 uint64_t canna_gettime()
@@ -11,4 +15,71 @@ uint64_t canna_gettime()
     t += tv.tv_usec / 1000;
 
     return t;
+}
+
+std::string canna_recv(zmq::socket_t &socket)
+{
+    zmq::message_t message;
+    socket.recv(&message);
+
+    return std::string(static_cast<char*>(message.data()), message.size());
+}
+
+bool canna_send(zmq::socket_t &socket, const std::string &string)
+{
+    zmq::message_t message(string.size());
+    memcpy(message.data(), string.data(), string.size());
+
+    bool result = socket.send(message);
+
+    return result;
+}
+
+bool canna_sendmore(zmq::socket_t &socket, const std::string &string)
+{
+    zmq::message_t message(string.size());
+    memcpy(message.data(), string.data(), string.size());
+
+    bool result = socket.send(message, ZMQ_SNDMORE);
+
+    return result;
+}
+
+void canna_dump(zmq::socket_t &socket)
+{
+    std::cout << "--------------------------------------------------" << std::endl;
+    while (true) {
+        zmq::message_t message;
+        socket.recv(&message);
+
+        int size = message.size();
+        std::string data(static_cast<char *>(message.data()), size);
+
+        bool is_text = true;
+        unsigned int count;
+        unsigned char c;
+        for (count = 0; count < size; count++) {
+            c = data[count];
+            if (c < 32 || c > 127) {
+                is_text = false;
+            }
+        }
+        std::cout << "[" << std::setfill('0') << std::setw(3) << size << "]";
+        for (count = 0; count < size; count++) {
+            if (is_text) {
+                std::cout << (char)data[count];
+            } else {
+                std::cout << std::setfill('0') << std::setw(2) << std::hex << (unsigned int)data[count];
+            }
+        }
+        std::cout << std::endl;
+
+        int more = 0;
+        size_t more_size = sizeof(more);
+        socket.getsockopt (ZMQ_RCVMORE, &more, &more_size);
+        
+        if (!more) {
+            break;
+        }
+    }
 }
--- core/canna_core.h
+++ core/canna_core.h
@@ -1,7 +1,15 @@
 #ifndef _CANNA_CORE_H
 #define _CANNA_CORE_H
 #include <stdint.h>
+#include <string>
+
+#include "zmq.hpp"
 
 uint64_t canna_gettime();
+
+std::string canna_recv(zmq::socket_t &socket);
+bool canna_send(zmq::socket_t &socket, const std::string &string);
+bool canna_sendmore(zmq::socket_t &socket, const std::string &string);
+void canna_dump(zmq::socket_t &socket);
 
 #endif
