d26d66627a848da453818f613472ec1fb4e168dd
apppur <applepurplediandian@gmail.com>
2016-05-31 13:29:12 +0800

    modify proto generator scripte

--- protos/make.sh
+++ protos/make.sh
@@ -1,2 +1,17 @@
-protoc -I=./ --cpp_out=./ ./addressbook.proto
-protoc -I=./ --cpp_out=./ ./nameinfo.proto
+#!/bin/sh
+
+#protoc -I=./ --cpp_out=./ ./addressbook.proto
+#protoc -I=./ --cpp_out=./ ./nameinfo.proto
+
+rm *.pb.h *.pb.cc 1>/dev/null 2>&1
+for i in *.proto
+do
+  protoc -I. --cpp_out=. $i
+  if [ "$?" -eq 0 ]
+  then
+    echo "generator $i.pb.h $i.pb.cc"
+  else
+    exit 1
+  fi
+done
+
